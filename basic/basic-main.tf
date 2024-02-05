704b58fa35b46b8ba7775ff134e8571a1c966bf7
iloveck <kan.ck.chen@gmail.com>
2024-02-05 07:20:38 -0800

    basic

--- basic/basic-main.tf
+++ basic/basic-main.tf
@@ -1,19 +1,6 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used.
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.74.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 # You can use the azurerm_client_config data resource to dynamically
 # extract connection settings from the provider configuration.
 
@@ -36,12 +23,6 @@ module "enterprise_scale" {
   }
 
   root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = "shc-1"
-  root_name      = "Standford Health Care 1"
-
-  deploy_identity_resources = true
-  subscription_id_identity  = "9bb108f8-b5b7-4ba5-96f7-65f17ee59b93"
-
-  deploy_management_resources = true
-  subscription_id_management  = "b1d61f03-47e1-4147-b8e8-ba6dc7092bed"
+  root_id        = var.root_id
+  root_name      = var.root_name
 }
